cb09ae06ef511ae8e7dc69ed721e050c2dfb66df
Diego <diego.kastan@gmail.com>
2026-06-02 17:03:50 -0600

    Initial Commit

---
+++ NutriApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NutriApp
+//
+//  Created by DIEGO CASTAÑEDA on 02/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NutriApp/NutriAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriAppApp.swift
+//  NutriApp
+//
+//  Created by DIEGO CASTAÑEDA on 02/06/26.
+//
+
+import SwiftUI
+
+@main
+struct NutriAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
